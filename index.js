3e192f2c7f9e062df2e2e9040c40ee5bae857784
KitaGaither <lakitgai@amazon.com>
2022-07-21 18:31:00 -0700

    Added destinations to destinationsDB

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ import { filterDestinations } from "./helpers.js";
 
 const server = express(); // This server is deaf
 
+// Middleware setup
 server.use(cors());
+server.use(express.json());
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -37,6 +40,21 @@ const destinationsDB = {
       "https://images.unsplash.com/photo-1564507592333-c60657eea523?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
 
 },
+123451: {
+    destination: "Kigali Arena",
+    location: "Kigali Rwanda",
+    photo: "https://images.unsplash.com/photo-1648708511872-5426c0f29c27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1531&q=80"
+}, 
+    123452: {
+        destination: "Taj Mahal",
+        location: "Agra India",
+        photo: "https://images.unsplash.com/photo-1564507592333-c60657eea523?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+    },
+    123453: {
+        destination: "Downtown Shibuya",
+        location: "Shibuya Japan",
+        photo: "https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }
 };
 
 
@@ -70,6 +88,8 @@ server.get("/destinations/city/:myCity", (req, res) => {
 server.get("/destinations/city/:myCity", (req, res) => {
     // log the city passed in the url as a named route parameter
     const city = req.params.myCity;
+    console.log(city);
+
 
     filterDestinations({ city, destinationsDB, res});
 });
@@ -77,6 +97,9 @@ server.get("/destinations/city/:myCity", (req, res) => {
 
 
 // UPDATE (OPTIONAL)
+server.post("/destinations", (req, res) => {
+console.log(req.body);
+});
 
 
 // DELETE (OPTIONAL)
